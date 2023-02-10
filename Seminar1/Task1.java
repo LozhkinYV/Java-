64559adbd7171e710f392385f98da39e05a51102
Yakov Lozhkin <yasher89@mail.ru>
2023-02-10 16:00:51 +0400

    Seminar1

---
+++ Seminar1/Task1.java
@@ -0,0 +1,40 @@
+package Seminar1;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        int[] array = randArray(10, 2);
+        System.out.println(Arrays.toString(array));
+        int[] invertArray = invertedArray(array );
+        System.out.println(Arrays.toString(invertArray));
+    }
+
+    private static int[] randArray(int len, int value) {
+
+        Random r = new Random();
+        int[] ints = new int[len];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = r.nextInt(value); // random [0, value)
+        }
+        return ints;
+    }
+
+
+    private static int[] invertedArray(int[] ints) {
+        int[] inverInts = new int[ints.length]; 
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] == 1){
+                inverInts[i] = 0;
+            }
+            else {
+                inverInts[i] = 1;
+            }
+            
+        }
+        return inverInts;
+    }
+
+}
