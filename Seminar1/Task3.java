21b5384bf2d24da775b25b470c8eb7dccffa6566
Yakov Lozhkin <yasher89@mail.ru>
2023-02-10 17:47:54 +0400

    Task3_1

--- Seminar1/Task3.java
+++ Seminar1/Task3.java
@@ -4,21 +4,17 @@
 //                                                                  то есть [0][0], [1][1], [2][2], …, [n][n];
 package Seminar1;
 
-
 import java.util.Arrays;
-import java.util.Random;
-
 
 public class Task3 {
     public static void main(String[] args) {
         int[][] twoDimArray = twoDimArray(10);
         printTwoDimArray(twoDimArray);
-        
+
     }
 
-
     private static int[][] twoDimArray(int len) {
-        int[][] twoInts = new int[len][len]; 
+        int[][] twoInts = new int[len][len];
         for (int i = 0; i < twoInts.length; i++) {
             twoInts[i][i] = 1;
             twoInts[i][twoInts.length - 1 - i] = 1;
@@ -26,12 +22,10 @@ public class Task3 {
         return twoInts;
     }
 
-
     private static void printTwoDimArray(int[][] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.println(Arrays.toString(array[i]));
         }
-        
+
     }
-     
 }
