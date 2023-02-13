125bea4dd056a8e56c68382c1dc55cf77554c442
Yakov Lozhkin <yasher89@mail.ru>
2023-02-13 09:25:55 +0400

    Task5_1

--- Seminar1/Task5.java
+++ Seminar1/Task5.java
@@ -10,13 +10,23 @@ public class Task5 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите значение a: ");
-        int a = in.nextInt();
-        System.out.print("Введите значение b: ");
-        int b = in.nextInt();
-        in.close();
-        int c = a + b;
-        System.out.println("Сумма значений a + b = " + c + " в интервале (10, 20): " + isSumBetween10And20(a, b));
-
+        if (in.hasNextInt()) {
+            int a = in.nextInt();
+            System.out.print("Введите значение b: ");
+            if (in.hasNextInt()) {
+                int b = in.nextInt();
+                in.close();
+                int c = a + b;
+                System.out.println("Сумма значений a + b = " + c + " в интервале (10, 20): " + isSumBetween10And20(a, b));
+            } 
+            else {
+                System.out.println("Это не число!");
+            }
+        } 
+        else {
+            System.out.println("Это не число!");
+        }
+        
     }
 
 private static boolean isSumBetween10And20(int a, int b) {
